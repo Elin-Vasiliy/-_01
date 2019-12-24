b5fd34a5f50da466c4af00a5b273e323ff5bd963
Elin-Vasiliy <Elinvasiliy@mail.ru>
2019-12-24 16:24:04 +0400

    Начало.

---
+++ Assets/Scripts/CreateGround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGround : MonoBehaviour
+{
+    public GameObject ground;
+    int height = 100;
+    int width = 100;
+
+    private void Awake()
+    {
+        SetGround();
+    }
+
+    void SetGround()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Vector3 vec = new Vector3();
+                GameObject grounds = Instantiate(ground);
+                grounds.transform.position = new Vector3(i, 0, j);
+                grounds.transform.parent = this.transform;
+
+            }
+        }
+    }
+}
